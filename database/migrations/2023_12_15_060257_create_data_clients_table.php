e74eeecc7d6114de159c6574bdebd4ef6e3afee5
gio-jac <giovanny.jacobo@gmail.com>
2023-12-21 12:17:00 -0600

    email y telefono en clientes, implementacion tcpdf

--- database/migrations/2023_12_15_060257_create_data_clients_table.php
+++ database/migrations/2023_12_15_060257_create_data_clients_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('client_id');
             $table->string('address')->default("N/A");
             $table->string('contact')->default("N/A");
+            $table->string('email')->default("N/A");
+            $table->string('phone')->default("N/A");
             $table->boolean('is_active')->default(true);
             $table->timestamps();
             $table->foreign('client_id')->references('id')->on('clients');
