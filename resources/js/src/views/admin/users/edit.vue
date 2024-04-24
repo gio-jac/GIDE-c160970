5723efa69cece7bad1f54b76a5919c04a8711bb7
gio-jac <giovanny.jacobo@gmail.com>
2024-04-24 16:51:28 -0600

    Fix switch de habilitar traduccion

--- resources/js/src/views/admin/users/edit.vue
+++ resources/js/src/views/admin/users/edit.vue
@@ -157,6 +157,21 @@
                         </p>
                     </template>
                 </div>
+                <div>
+                    <label for="formActive">{{ $t("user.create.activeUser") }}</label>
+                    <label class="w-12 h-6 relative">
+                        <input
+                            type="checkbox"
+                            class="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer"
+                            id="formActive"
+                            v-model="form.is_active"
+                        />
+                        <span
+                            for="formActive"
+                            class="bg-[#ebedf2] dark:bg-dark block h-full before:absolute before:left-1 before:bg-white dark:before:bg-white-dark dark:peer-checked:before:bg-white before:bottom-1 before:w-4 before:h-4 peer-checked:before:left-7 peer-checked:bg-primary before:transition-all before:duration-300"
+                        ></span>
+                    </label>
+                </div>
                 <button type="submit" class="btn btn-primary !mt-6">{{ $t("user.create.update") }}</button>
             </form>
         </div>
