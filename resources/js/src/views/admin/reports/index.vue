90fd051c0edaf05434796ca26f4bcbd25ddf5ed2
gio-jac <giovanny.jacobo@gmail.com>
2024-04-25 12:54:04 -0600

    Traducciones index de reportes

--- resources/js/src/views/admin/reports/index.vue
+++ resources/js/src/views/admin/reports/index.vue
@@ -29,7 +29,7 @@
                                     <line x1="12" y1="5" x2="12" y2="19"></line>
                                     <line x1="5" y1="12" x2="19" y2="12"></line>
                                 </svg>
-                                Add New
+                                {{ $t("report.index.newReport") }}
                             </Link>
                         </div>
                         <div class="ltr:ml-auto rtl:mr-auto">
@@ -37,7 +37,7 @@
                                 v-model="search"
                                 type="text"
                                 class="form-input"
-                                placeholder="Search..."
+                                :placeholder="$t('user.index.searchPlaceholder')"
                             />
                         </div>
                     </div>
@@ -211,12 +211,14 @@
     </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import Vue3Datatable from "@bhplugin/vue3-datatable";
 import AppLayout from "@/layouts/app-layout.vue";
 import SiteLayout from "@/layouts/app.vue";
 import { Head, Link, router } from "@inertiajs/vue3";
+import { useI18n } from 'vue-i18n';
 
+const { t } = useI18n();
 defineOptions({
     layout: [SiteLayout, AppLayout],
 });
@@ -231,15 +233,15 @@ console.log(props.reports);
 
 const datatable: any = ref(null);
 const search = ref("");
-const cols = ref([
+const cols = computed(() =>[
     { field: "id", title: "ID" },
-    { field: "complete_id", title: "File Name" },
-    { field: "status", title: "Status" },
-    { field: "assigned_to", title: "Assigned To" },
-    { field: "created_at", title: "Creation Date" },
+    { field: "complete_id", title: t("report.index.col.fileName") },
+    { field: "status", title: t("report.index.col.status") },
+    { field: "assigned_to", title: t("report.index.col.assigned") },
+    { field: "created_at", title: t("report.index.col.creationDate") },
     {
         field: "actions",
-        title: "Actions",
+        title: t("user.index.col.actions"),
         sort: false,
         headerClass: "justify-center",
     },
