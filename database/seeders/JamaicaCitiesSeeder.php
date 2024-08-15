7ebf3ef36ac068f62f4336bb92d793d9dc29e491
gio-jac <giovanny.jacobo@gmail.com>
2024-08-15 13:36:48 -0600

    Datos de ciudades de jamaica

--- database/seeders/JamaicaCitiesSeeder.php
+++ database/seeders/JamaicaCitiesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\machine_reports\City;
 
 class JamaicaCitiesSeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class JamaicaCitiesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $city = 5;
+        City::create(['name' => 'Kingston', 'country_id' => $city]);
+        City::create(['name' => 'Portmore', 'country_id' => $city]);
+        City::create(['name' => 'Montego Bay', 'country_id' => $city]);
+        City::create(['name' => 'Spanish Town', 'country_id' => $city]);
+        City::create(['name' => 'May Pen', 'country_id' => $city]);
+        City::create(['name' => 'Mandeville', 'country_id' => $city]);
+        City::create(['name' => 'Old Harbour', 'country_id' => $city]);
+        City::create(['name' => 'Savanna-la-Mar', 'country_id' => $city]);
+        City::create(['name' => 'Ocho Rios', 'country_id' => $city]);
+        City::create(['name' => 'Linstead', 'country_id' => $city]);
+        City::create(['name' => 'Port Antonio', 'country_id' => $city]);
+        City::create(['name' => "Saint Ann's Bay", 'country_id' => $city]);
+        City::create(['name' => 'Morant Bay', 'country_id' => $city]);
+        City::create(['name' => 'Yallahs', 'country_id' => $city]);
+        City::create(['name' => 'Hayes', 'country_id' => $city]);
+        City::create(['name' => 'Santa Cruz', 'country_id' => $city]);
     }
 }
