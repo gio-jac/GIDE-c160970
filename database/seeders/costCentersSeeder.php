9bda1237a0ba7b4e075e333e47430eaf224b6689
DavidSanchezVillalobos <84885271+DavidSanchezVillalobos@users.noreply.github.com>
2024-08-27 13:26:50 -0500

    correccion del nombre del seeder

--- database/seeders/costCentersSeeder.php
+++ database/seeders/costCentersSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class costCentersSeeder extends Seeder
+class CostCentersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
