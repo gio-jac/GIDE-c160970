847e50b7971882cb23a694e4d67113ae663628e2
gio-jac <129218147+gio-jac@users.noreply.github.com>
2025-10-09 15:40:56 -0600

    Bugfix/mrep traducciones catalogos es pt (#186)

--- database/seeders/FailureTypeSeeder.php
+++ database/seeders/FailureTypeSeeder.php
@@ -13,21 +13,21 @@ class FailureTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        FailureType::create(['name' => 'ADJUSTMENT']);
-        FailureType::create(['name' => 'UPDATE']);
-        FailureType::create(['name' => 'CHANGE OIL']);
-        FailureType::create(['name' => 'UNISNTALLATION']);
-        FailureType::create(['name' => 'ADJUST CONNECTORS']);
-        FailureType::create(['name' => 'INSTALLATION']);
-        FailureType::create(['name' => 'CLEANING']);
-        FailureType::create(['name' => 'REINSTALLATION']);
-        FailureType::create(['name' => 'RESET']);
-        FailureType::create(['name' => 'REPLACE PARTS']);
-        FailureType::create(['name' => 'PREVENTIVE']);
-        FailureType::create(['name' => 'UNPRODUCTIVE']);
-        FailureType::create(['name' => 'TRAINING']);
-        FailureType::create(['name' => 'PREVENTIVE MAINTENANCE, 500 HRS']);
-        FailureType::create(['name' => 'PREVENTIVE MAINTENANCE, 1000 HRS']);
-        FailureType::create(['name' => 'PREVENTIVE MAINTENANCE, 2000 HRS']);
+        FailureType::create(['name' => 'ADJUSTMENT', 'es' => 'AJUSTE', 'pt' => 'AJUSTES']);
+        FailureType::create(['name' => 'UPDATE', 'es' => 'ACTUALIZAR', 'pt' => 'ATUALIZACAO']);
+        FailureType::create(['name' => 'CHANGE OIL', 'es' => 'CAMBIO ACEITE', 'pt' => 'COMPLETAR OLEO']);
+        FailureType::create(['name' => 'UNINSTALLATION', 'es' => 'DESINSTALACIÓN', 'pt' => 'DESISNTALACAO']);
+        FailureType::create(['name' => 'ADJUST CONNECTORS', 'es' => 'AJUSTAR CONECTORES', 'pt' => 'ELIMINADO MAU CONTATO']);
+        FailureType::create(['name' => 'INSTALLATION', 'es' => 'INSTALACIÓN', 'pt' => 'INSTALACAO']);
+        FailureType::create(['name' => 'CLEANING', 'es' => 'LIMPIAR', 'pt' => 'LIMPEZA']);
+        FailureType::create(['name' => 'REINSTALLATION', 'es' => 'REINSTALACIÓN', 'pt' => 'REINSTALACAO']);
+        FailureType::create(['name' => 'RESET', 'es' => 'REINICIAR', 'pt' => 'RESET']);
+        FailureType::create(['name' => 'REPLACE PARTS', 'es' => 'SUSTITUCIÓN DE PIEZAS', 'pt' => 'SUBSTITUICAO DE PECA']);
+        FailureType::create(['name' => 'PREVENTIVE', 'es' => 'PREVENTIVO', 'pt' => 'PREVENTIVA']);
+        FailureType::create(['name' => 'UNPRODUCTIVE', 'es' => 'NO PRODUCTIVO', 'pt' => 'IMPRODUTIVO']);
+        FailureType::create(['name' => 'TRAINING', 'es' => 'ENTRENAMIENTO', 'pt' => 'TREINAMENTO']);
+        FailureType::create(['name' => 'PREVENTIVE MAINTENANCE, 500 HRS', 'es' => 'MANTENIMIENTO PREVENTIVO, 500 HRS', 'pt' => 'MANUTENÇÃO PREVENTIVA, 500 HRS']);
+        FailureType::create(['name' => 'PREVENTIVE MAINTENANCE, 1000 HRS', 'es' => 'MANTENIMIENTO PREVENTIVO, 1000 HRS', 'pt' => 'MANUTENÇÃO PREVENTIVA, 1000 HRS']);
+        FailureType::create(['name' => 'PREVENTIVE MAINTENANCE, 2000 HRS', 'es' => 'MANTENIMIENTO PREVENTIVO, 2000 HRS', 'pt' => 'MANUTENÇÃO PREVENTIVA, 2000 HRS']);
     }
 }
