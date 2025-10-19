31f1b6706084d612499382b10c5635ac9142cad9
gio-jac <giovanny.jacobo@gmail.com>
2025-10-19 17:28:25 -0600

    chore(mrep): añade relaciones entre reporte y visit

---
+++ app/Models/machine_reports/ServiceVisit.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\machine_reports;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ServiceVisit extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'complete_id',
+        'user_id',
+        'shift_id',
+        'branch_id',
+        'branch_manager_id',
+        'service_date',
+        'service_timezone',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'service_date' => 'date',
+        'is_active'    => 'bool',
+    ];
+
+    public function shift(): BelongsTo
+    {
+        return $this->belongsTo(Shift::class);
+    }
+
+    public function branch(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function branchManager(): BelongsTo
+    {
+        return $this->belongsTo(BranchManager::class);
+    }
+    
+    public function serviceReports(): HasMany
+    {
+        return $this->hasMany(ServiceReport::class, 'service_visit_id');
+    }
+}
